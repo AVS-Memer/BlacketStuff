9882507a5c3730eef2b92499455c44cccfd95c9f
AVS <123315354+AVS-Memer@users.noreply.github.com>
2023-10-29 14:28:11 -0400

    Update RarityCounter.js

--- RarityCounter.js
+++ RarityCounter.js
@@ -1,12 +1,20 @@
 let rarity = prompt("Which rarity do you want to find? (Uncommon, Rare, ..., Mystical)");
 let count = 0;
 let total = 0;
-for (let i of blacket.blooks) {
-  if (i.rarity == rarity) {
-    if (blacket.user.blooks.includes(i)) {
+let totalBlooks = [];
+for (let key in blacket.blooks) {
+  totalBlooks.push(key);
+}
+let userBlooks = [];
+for (let key in blacket.user.blooks) {
+  userBlooks.push(key);
+}
+for (let i of totalBlooks) {
+  if (blacket.blooks[i].rarity == rarity) {
+    if (userBlooks.includes(i)) {
       count++;
     }
     total++;
   }
 }
-alert("You have " + count + " / " total + " " + rarity + "s.");
+alert("You have " + count + " / " + total + " " + rarity + "s.");
