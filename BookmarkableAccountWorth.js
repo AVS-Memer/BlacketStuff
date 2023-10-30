14f44f34845e62da518fcccd2700bb64b93406b8
AVS <123315354+AVS-Memer@users.noreply.github.com>
2023-10-30 17:38:27 -0400

    Bookmarkable AccountWorth.js

---
+++ BookmarkableAccountWorth.js
@@ -0,0 +1,1 @@
+let obtained = 0; let blooks = []; for (let key in blacket.user.blooks) {blooks.push(key);} for (let i of blooks) {obtained += blacket.blooks[i].price*blacket.user.blooks[i];} let total = obtained + blacket.user.tokens; alert("Selling all of your blooks will result in obtaining " + obtained + " tokens, resulting in a total of " + total);
