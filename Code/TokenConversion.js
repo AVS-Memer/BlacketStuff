a8d0fd44c161f0e4154ec008a1397c0d8535efb0
AVS <123315354+AVS-Memer@users.noreply.github.com>
2024-09-23 11:10:42 -0400

    Update TokenConversion.js

--- Code/TokenConversion.js
+++ Code/TokenConversion.js
@@ -1,19 +1,19 @@
 let tokens = prompt("How many tokens?");
 let conversion = prompt("What should the tokens be converted into?");
-let conversions = ["dollars","Dollars","USD","1 Hour Boosters","1 hour booster","1hr Booster","1hr booster","3 Hour Boosters","3 hour booster","3hr Booster","3hr booster","Plus","plus"];
-while (!conversions.includes(conversion)) {
+let conversions = ["dollars","usd","1 hour boosters","1 hour booster","1hr booster","1hr boosters","3 hour boosters","3 hour booster","3hr booster","3hr booster","Plus","plus"];
+while (!conversions.includes(conversion.toLowerCase())) {
   conversion = prompt("That is not an acceptable answer. What should the tokens be converted into?");
 }
-if (conversions.slice(0,3).includes(conversion)) {
+if (conversions.slice(0,2).includes(conversion.toLowerCase())) {
   let dollars = Math.floor(tokens/100)/100;
   alert(tokens + " tokens is equivalant to " + dollars + " dollars.");
-} else if (conversions.slice(3,8).includes(conversion)) {
+} else if (conversions.slice(2,6).includes(conversion.toLowerCase())) {
   let booster = Math.floor(tokens/50000);
   alert(tokens + " tokens is equivalant to buying " + booster + " 1 Hour Boosters.");
-} else if (conversions.slice(8,11).includes(conversion)) {
+} else if (conversions.slice(6,10).includes(conversion.toLowerCase())) {
   let booster = Math.floor(tokens/100000);
   alert(tokens + " tokens is equivalant to buying " + booster + " 3 Hour Boosters.");
-} else if (conversions.slice(11,13).includes(conversion)) {
+} else if (conversions.slice(10).includes(conversion.toLowerCase())) {
   let plus = Math.floor(tokens/100000);
   alert(tokens + " tokens is equivalant to buying Blacket Plus " + plus + "times.");
 }
